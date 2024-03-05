aed38f2a6e94d7cffe9a0f55555127148a4e9b9f
johansromeroml <johansebastian.romero@mercadolibre.com.co>
2024-03-05 15:12:15 -0500

    added functionalities

--- internal/tickets/tickets.go
+++ internal/tickets/tickets.go
@@ -1,13 +1,72 @@
 package tickets
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Ticket struct {
+	ID      int
+	Name    string
+	Email   string
+	Country string
+	Hour    string
+	Price   int
+}
+
+type Tickets struct {
+	Tickets []Ticket
+}
+
+func TicketFromString(in string) Ticket {
+	//println("--", in)
+	attributes := strings.Split(in, ",")
+	//println(attributes)
+	id, _ := strconv.Atoi(attributes[0])
+	price, _ := strconv.Atoi(attributes[5])
+	return Ticket{ID: id, Name: attributes[1], Email: attributes[2], Country: attributes[3], Hour: attributes[4], Price: price}
 }
 
 // ejemplo 1
-func GetTotalTickets(destination string) (int error) {}
+func GetTotalTickets(destination string, t *Tickets) (total int, err error) {
+	total = 0
+	for _, ticket := range t.Tickets {
+		if ticket.Country == destination {
+			total++
+		}
+	}
+	return
+}
 
 // ejemplo 2
-func GetMornings(time string) (int error) {}
+func GetTicketsByDayframe(t *Tickets) (a, b, c, d int, err error) {
+	a = 0
+	b = 0
+	c = 0
+	d = 0
+	for _, ticket := range t.Tickets {
+		tim := strings.Split(ticket.Hour, ":")
+		hours, _ := strconv.Atoi(tim[0])
+		//mins, _ := strconv.Atoi(tim[1])
+		switch {
+		case hours <= 6:
+			a++
+		case hours >= 7 && hours <= 12:
+			b++
+		case hours >= 13 && hours <= 19:
+			c++
+		case hours >= 20:
+			d++
+		}
+	}
+	return
+}
 
 // ejemplo 3
-func AverageDestination(destination string, total int) (int error) {}
+func AverageDestination(destination string, t *Tickets) (avg float64, err error) {
+	flightsToDestination, err := GetTotalTickets(destination, t)
+	totalTickets := len(t.Tickets)
+	//println("f2d: ", flightsToDestination, ", tut: ", totalTickets)
+	avg = float64(flightsToDestination) * 100 / float64(totalTickets)
+	return
+}
--- main.go
+++ main.go
@@ -1,9 +1,65 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
 	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
 )
 
 func main() {
-	total, err := tickets.GetTotalTickets("Brazil")
+	ticketsCSV, err := os.Open("tickets.csv")
+	defer func(f *os.File) {
+		f.Close()
+		fmt.Println("End of execution")
+
+	}(ticketsCSV)
+
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Println("ticketsCSV: ", ticketsCSV)
+
+	ticketsData, err := io.ReadAll(ticketsCSV)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fmt.Println("ticketsData: ", string(ticketsData))
+
+	ticketsPreSlice := strings.Split(string(ticketsData), "\n")
+
+	//fmt.Println("tickets Pre slice: ", ticketsPreSlice)
+	ticketList := tickets.Tickets{}
+
+	for _, ticketPre := range ticketsPreSlice[:len(ticketsPreSlice)-1] {
+		tick := tickets.TicketFromString(ticketPre)
+		//fmt.Println(tick)
+		ticketList.Tickets = append(ticketList.Tickets, tick)
+	}
+
+	//fmt.Println(ticketList.Tickets[1])
+
+	totalTickets, err := tickets.GetTotalTickets("Brazil", &ticketList)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Numero total de tickets : ", totalTickets)
+
+	earlyMorning, morning, afternoon, evening, err := tickets.GetTicketsByDayframe(&ticketList)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Tickets de madrugada: ", earlyMorning)
+	fmt.Println("Tickets de mañana: ", morning)
+	fmt.Println("Tickets de tarde: ", afternoon)
+	fmt.Println("Tickets de noche: ", evening)
+	//fmt.Println("tut", earlyMorning+morning+afternoon+evening)
+
+	avgToBrazil, err := tickets.AverageDestination("Brazil", &ticketList)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Average tickets to Brazil: ", avgToBrazil)
 }
